711d3278c8008e0ea92d054077a6b74b7c458669
William Sutcliffe <william.sutcliffe@cern.ch>
2024-08-18 17:49:56 +0200

    first version of weighted message parsing gnn

--- src/blocks/abstract_module.py
+++
@@ -1,9 +0,0 @@
-
-
-class AbstractModule(nn.Module):
-    def __init__(self, *args, **kwargs):
-        super(AbstractModule, self).__init__()
-
-    @contextlib.contextmanager
-    def _enter_variable_scope(self, *args, **kwargs):
-        yield None
