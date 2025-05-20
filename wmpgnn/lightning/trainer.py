b46d962de88cb2aa9a4185376f2cf49c813ea970
yukai-zh <yukai.zhao4@gmail.com>
2025-05-20 12:57:03 +0200

    bug fix

--- wmpgnn/lightning/trainer.py
+++ wmpgnn/lightning/trainer.py
@@ -1,8 +1,10 @@
-import torch
-
-from torch_geometric.loader import DataLoader
-
+import sys
+import os
+import glob
 from optparse import OptionParser
+
+import torch
+from torch_geometric.loader import DataLoader
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
 from wmpgnn.lightning.lightning_module import training
@@ -30,12 +32,18 @@ if __name__ == "__main__":
     model = model_loader.get_model()
 
     # Get the dataset glob it and load
-    trn_dataset = torch.load("training_data_4000.pt", weights_only=False)
-    val_dataset = torch.load("validation_data_1000.pt", weights_only=False)
+    trn_paths = sorted(glob.glob(f'{option.INDIR}/training_data_*'))
+    trn_dataset = []
+    for path in trn_paths:
+        trn_dataset.append(torch.load(path, weights_only=False))
+    val_paths = sorted(glob.glob(f'{option.INDIR}/validation_data_*'))
+    val_dataset = []
+    for path in val_paths:
+        val_paths.append(torch.load(path, weights_only=False))
 
-    # here we can check what kind of gpu it is
-    trn_loader = DataLoader(trn_dataset, batch_size=6, num_workers=15, drop_last=True) 
-    val_loader = DataLoader(val_dataset, batch_size=6, num_workers=15, drop_last=True) 
+    # here we can check what kind of gpu it is to specify bs, also num_workers = num_cpu * 2
+    trn_loader = DataLoader(trn_dataset, batch_size=6, num_workers=8, drop_last=True) 
+    val_loader = DataLoader(val_dataset, batch_size=6, num_workers=8, drop_last=True) 
 
     # here we can do two stuff loading or during training
     if option.CW:  # CW = calculate pos weights
@@ -44,4 +52,4 @@ if __name__ == "__main__":
         pos_weight = {'t_nodes': torch.tensor(11.8125),'tt_edges': torch.tensor(573.6347), 'LCA': torch.tensor([2.5022e-01, 4.8136e+02, 7.8440e+02, 7.4500e+03]) }
     print(pos_weight)
     
-    training(model, pos_weight, 10, 1, trn_loader, val_loader) # need to add gradient accumulation
+    training(model, pos_weight, 1, 1, trn_loader, val_loader) # need to add gradient accumulation
